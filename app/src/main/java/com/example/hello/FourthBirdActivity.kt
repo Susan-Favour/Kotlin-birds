b001cde0f131840e1aafad8b2f53edceabd5e7d8
SusanFavour <susanfavourodongo@gmail.com>
2024-06-12 20:08:21 +0300

    add changes

--- app/src/main/java/com/example/hello/FourthBirdActivity.kt
+++ app/src/main/java/com/example/hello/FourthBirdActivity.kt
@@ -7,24 +7,33 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.hello.databinding.FourthBirdBinding
+import com.squareup.picasso.Picasso
 
 class FourthBirdActivity : AppCompatActivity() {
+    lateinit var binding: FourthBirdBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.fourth_bird)
 
-        val btnPrev3 = findViewById<ImageView>(R.id.btnPrev3)
-        val btnNext4 = findViewById<ImageView>(R.id.btnNext4)
+        binding = FourthBirdBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        btnPrev3.setOnClickListener{
+
+
+        binding.btnPrev3.setOnClickListener{
             finish()
         }
 
-        btnNext4.setOnClickListener{
+        binding.btnNext4.setOnClickListener{
             val intent = Intent ( this, FifthBirdActivity::class.java )
             startActivity(intent)
         }
+        Picasso
+            .get()
+            .load("https://images.unsplash.com/photo-1445820200644-69f87d946277?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fGJpcmRzfGVufDB8fDB8fHww")
+            .centerCrop()
+            .resize(500,500)
+            .into(binding.imageView5)
     }
 }
